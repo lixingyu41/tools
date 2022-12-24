2926ef89a484e302ce8d1217b2a0d9d8fa68633b
Xingyu Li <415758212@qq.com>
2022-12-25 02:02:23 +0800

    Initial commit

---
+++ p/scripts/bhget.js
@@ -0,0 +1,65 @@
+window.onload=function(){
+    when_load();
+}
+// 信息获取
+function buttonget(){
+    //读取填写信息
+    dataget();
+    //检查填完没
+    blank_check();
+    //填完就执行数据存储
+    if(data_check){
+        datasave();
+    }
+}
+//数据读取
+function dataget(){
+    //性别
+    user_gender_get=document.getElementById("user_gender").value;
+    console.log(user_gender_get);
+    //生日
+    user_date_get=document.getElementById("user_date").value;
+    console.log(user_date_get);
+    //身高
+    user_tall_get=document.getElementById("user_tall").value;
+    console.log(user_tall_get);
+    //体重
+    user_weight_get=document.getElementById("user_weight").value;
+    console.log(user_weight_get);
+}
+//数据存储
+function datasave(){
+    localStorage.setItem('ltool_user_gender',user_gender_get);
+    localStorage.setItem('ltool_user_date',user_date_get);
+    localStorage.setItem('ltool_user_tall',user_tall_get);
+    localStorage.setItem('ltool_user_weight',user_weight_get);
+    localStorage.setItem('ltool_user_check',1);
+}
+//全量检查是否填写完整
+function blank_check(){
+    if(user_gender_get==''||user_date_get==''||user_tall_get==''||user_weight_get==''){
+        alert('信息填写不完整');
+        data_check=0;
+    }
+    else{
+        data_check=1;
+    }
+}
+//加载网页是读取本地储存，如有则赋给表格默认值
+function when_load(){
+    data_check=localStorage.getItem('ltool_user_check');
+    if(data_check){
+        document.getElementById("user_weight").value=localStorage.getItem('ltool_user_weight');
+        document.getElementById("user_tall").value=localStorage.getItem('ltool_user_tall')
+        document.getElementById("user_date").value=localStorage.getItem('ltool_user_date')
+        document.getElementById("user_gender").value=localStorage.getItem('ltool_user_gender')
+    }
+    age_zhou();
+    age_xu();
+    bmi();
+    water();
+    bodyfat();
+    heartrate();
+    bmr();
+    cal();
+}
---
+++ p/scripts/bhout.js
@@ -0,0 +1,104 @@
+//数据拉取
+user_weight_get=localStorage.getItem('ltool_user_weight');
+user_tall_get=localStorage.getItem('ltool_user_tall')
+user_date_get=localStorage.getItem('ltool_user_date')
+user_gender_get=localStorage.getItem('ltool_user_gender')
+//数据处理
+today = new Date();
+    now_year = today.getFullYear();
+    now_month = today.getMonth() + 1 ;
+    now_day = today.getDate();
+    console.log(now_year);
+    console.log(now_month);
+    console.log(now_day);
+userdate = new Date(user_date_get);
+    user_year = userdate.getFullYear();
+    user_month = userdate.getMonth() + 1 ;
+    user_day = userdate.getDate();
+    console.log(user_year);
+    console.log(user_month);
+    console.log(user_day);
+if(user_gender_get=="boy"){
+    user_gender_num = 1;
+}
+else if(user_gender_get=="girl"){
+    user_gender_num = 0;
+}
+//计算年龄
+//周岁
+function age_zhou(){
+    if(now_month > user_month){
+        user_age_zhou = now_year - user_year;
+    }
+    else if(now_month == user_month){
+        if(now_day > user_day){
+            user_age_zhou = now_year - user_year;
+        }
+    }
+    else{
+        user_age_zhou = now_year - user_year - 1;
+    }
+    console.log(user_age_zhou);
+    document.getElementById("user_age_zhou").innerHTML = user_age_zhou;
+}
+//虚岁
+function age_xu(){
+    user_age_xu = now_year - user_year + 1;
+    console.log(user_age_xu);
+    document.getElementById("user_age_xu").innerHTML = user_age_xu;
+}
+//BMI
+function bmi(){
+    user_bmi = user_weight_get / ( ( user_tall_get / 100 ) * ( user_tall_get / 100 ) )
+    console.log(user_bmi);
+    document.getElementById("user_bmi").innerHTML = user_bmi;
+    if(user_bmi<18.5){
+        user_bmi_sug = ("体重过低");
+    }
+    else if(user_bmi>=18.5&&user_bmi<24){
+        user_bmi_sug = ("正常范围");
+    }
+    else if(user_bmi>=24&&user_bmi<28){
+        user_bmi_sug = ("肥胖前期");
+    }
+    else if(user_bmi>=28&&user_bmi<30){
+        user_bmi_sug = ("I度肥胖");
+    }
+    else if(user_bmi>=30&&user_bmi<40){
+        user_bmi_sug = ("II度肥胖");
+    }
+    else if(user_bmi>=40){
+        user_bmi_sug = ("III度肥胖");
+    }
+    document.getElementById("user_bmi_sug").innerHTML = user_bmi_sug;
+}
+//饮水量
+function water(){
+    user_water = user_weight_get * 35;
+    document.getElementById("user_water").innerHTML = user_water;
+}
+//体脂估算
+function bodyfat(){
+    user_bodyfat = (1.2 * user_bmi) + (0.23 * user_age_zhou) - 5.4 - (10.8 * user_gender_num);
+    document.getElementById("user_bodyfat").innerHTML = user_bodyfat;
+}
+//心率区间
+function heartrate(){
+    user_heartrate_fast = 205.8 - (0.685 * user_age_zhou );
+    document.getElementById("user_heartrate_fast").innerHTML = user_heartrate_fast;
+}
+//代谢
+function bmr(){
+    if(user_gender_num){
+        user_bmr = 66 + (13.7 * user_weight_get) + (5 * user_tall_get) - (6.8 * user_age_zhou);
+    }
+    else if(!user_gender_num){
+        user_bmr = 655 + (9.6 * user_weight_get) + (1.8 * user_tall_get) - (4.7 * user_age_zhou);
+    }
+    document.getElementById("user_bmr").innerHTML = user_bmr;
+}
+//卡路里
+function cal(){
+    user_cal = user_weight_get * 33;
+    document.getElementById("user_cal").innerHTML = user_cal;
+}
